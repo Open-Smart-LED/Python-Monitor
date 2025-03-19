add3decee192ffc82ad6a86ef75122a13b887923
Zibraltar-XIX <jparola@guardiaschool.fr>
2025-03-19 22:38:53 +0100

    first connection

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import time
 from api_meteo import get_weather
+import socket
+esp_ip = "192.168.4.1"
+port = 2025
 end = "not"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((esp_ip, port))
+print(f"Connecté à l'ESP32 ({esp_ip}:{port})")
+message = "Hello ESP32!\n"  # On ajoute '\n' car l'ESP attend une fin de ligne
+sock.sendall(message.encode())  # Envoie en binaire
+
+print(f"Message envoyé: {message}")
+
+# Fermeture de la connexion
+sock.close()
 
 def check_meteo():
     get_weather
